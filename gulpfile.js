df9775d5792f36d2917e5c683af6672812c56631
ldkrsi <louie810405@livemail.tw>
2023-06-16 22:37:21 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import gulp from 'gulp';
-import dartSass from 'sass';
+import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
